113036769d5b13ad5cff612ff8dcd97098b7cec2
Yash Gupta <yash16022001@gmail.com>
2024-03-06 23:21:35 +0530

    ..

--- srcTs/interfaces/global.d.ts
+++
@@ -1,9 +0,0 @@
-import { UserEntity } from "../Database/Entities/user.entity";
-
-declare global {
-  namespace Express {
-    export interface User extends Partial<UserEntity> {}
-
-    export interface Request {}
-  }
-}
